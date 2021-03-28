e33d25aca5e87f2d651bd170bfb4d484a845c2eb
Tali <talisch@post.bgu.ac.il>
2021-03-28 20:52:15 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const App = () => {
+  return (
+    // <SafeAreaView style={backgroundStyle}>
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //     <Header />
+        <View>
+             <Text>
+                Hello
+              </Text> 
+        </View>
+    //   </ScrollView>
+    // </SafeAreaView>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+export default App;
